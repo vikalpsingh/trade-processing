50caddf1ada474b4d6a9170bbe9583c1a059797f
Vikalp <vikalp.singh@gmail.com>
2023-09-28 10:42:21 +0530

    Test cases fixing with help of Mocito

--- src/test/java/com/db/trade/processing/TradeStoreApplicationTest.java
+++ src/test/java/com/db/trade/processing/TradeStoreApplicationTest.java
@@ -28,21 +28,21 @@ class TradeStoreApplicationTest {
 
     @BeforeAll
     public static void setup() {
-        trades.add(new Trade("T1", 1, "CP-1", "B1", LocalDate.of(2022, 5, 20), LocalDate.now(), "N"));
-        trades.add(new Trade("T2", 2, "CP-2", "B1", LocalDate.of(2022, 5, 20), LocalDate.now(), "N"));
-        trades.add(new Trade("T2", 1, "CP-1", "B1", LocalDate.of(2022, 5, 20), LocalDate.of(2015, 5, 20), "N"));
-        trades.add(new Trade("T3", 3, "CP-3", "B2", LocalDate.of(2022, 5, 20), LocalDate.now(), "Y"));
+        trades.add(new Trade("T1", 1, "CP-1", "B1", LocalDate.of(2024, 5, 20), LocalDate.now(), "N"));
+        trades.add(new Trade("T2", 2, "CP-2", "B1", LocalDate.of(2024, 5, 20), LocalDate.now(), "N"));
+        trades.add(new Trade("T2", 1, "CP-1", "B1", LocalDate.of(2024, 5, 20), LocalDate.of(2015, 5, 20), "N"));
+        trades.add(new Trade("T3", 3, "CP-3", "B2", LocalDate.of(2024, 5, 20), LocalDate.now(), "Y"));
     }
 
     @Test
     void testTradeStoreTradeSignleVersion() {
         StoreStatus status = this.controller
-                .storeTrade(new Trade("T1", 1, "CP-1", "B1", LocalDate.of(2022, 5, 20), LocalDate.now(), "N"));
+                .storeTrade(new Trade("T1", 1, "CP-1", "B1", LocalDate.of(2024, 5, 20), LocalDate.now(), "N"));
 
         assertSame(StoreStatus.OK, status);
 
         status = this.controller
-                .storeTrade(new Trade("T1", 2, "CP-1", "B1", LocalDate.of(2022, 5, 20), LocalDate.now(), "N"));
+                .storeTrade(new Trade("T1", 2, "CP-1", "B1", LocalDate.of(2024, 5, 20), LocalDate.now(), "N"));
 
         assertSame(StoreStatus.OK, status);
     }
